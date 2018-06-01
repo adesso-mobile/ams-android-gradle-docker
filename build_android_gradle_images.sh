ac67235832608fd3afc34680679a739a73140566
Tim Dithmer <dithmer@adesso-mobile.de>
2018-06-01 16:18:53 +0200

    Changing to build

--- build_android_gradle_images.sh
+++ build_android_gradle_images.sh
@@ -18,36 +18,19 @@ android_sdk_tools_versions=(3859397)
 for gradle_version in ${gradle_versions[@]}; do
     for android_sdk_tools_version in ${android_sdk_tools_versions[@]}; do
         for android_build_tools_version in ${android_build_tools_versions[@]}; do
-            git checkout master
-            git status -s | grep -q . && { 
-                echo "Alter, krich dein Scheiss zam!" 
-                exit 1
-            }
-            git reset --hard
-
             android_major_version=${android_build_tools_version%%.*}
             android_target_sdk="android-$android_major_version"
             android_images="sys-img-armeabi-v7a-android-${android_major_version},sys-img-armeabi-v7a-android-${android_major_version}"
 
             version_string="$gradle_version-$android_build_tools_version-$android_sdk_tools_version"
 
-            branch_name="build/$version_string"
-
-            git_branch_exist "$branch_name" && { 
-                git checkout "$branch_name"
-                git merge master --no-edit
-                continue
-            }
-
-            git checkout -b "$branch_name"
-            mkdir -p buildenv
-            echo "$gradle_version" > buildenv/gradle_version
-            echo "$android_target_sdk" > buildenv/android_target_sdk
-            echo "$android_build_tools_version" > buildenv/android_build_tools_version
-            echo "$android_sdk_tools_version" > buildenv/android_sdk_tools_version
-            echo "$android_images" > buildenv/android_images
-
-            git add -A && git commit -m "New Build-Branch for $version_string" . && git push -u origin "$branch_name"
+            docker build -t "ams-android-gradle:$version_string" \
+                --build-arg "gradle_version=$gradle_version" \
+                --build-arg "android_target_sdk=$android_target_sdk" \
+                --build-arg "android_build_tools=$android_build_tools_version" \
+                --build-arg "android_sdk_tools_version=$android_sdk_tools_version" \
+                --build-arg "android_images=$android_images" \
+                .
         done
     done
 done
