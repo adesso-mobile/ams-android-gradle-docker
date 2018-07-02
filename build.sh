ec9502d9eacff197343b7acb3ed148319c847163
Tim Dithmer <dithmer@adesso-mobile.de>
2018-07-02 10:12:45 +0200

    Removing gradle user foo

--- build.sh
+++ build.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-"$(pwd)"/gradlew $GRADLE_TASKS
+test -n "$NEW_UID" || exit 1
+useradd -d /opt/workspace -u "$NEW_UID" -g "$NEW_UID" teamcity
+
 chown "$NEW_UID:$NEW_UID" /opt/workspace -R
+
+su - teamcity
+
+"$(pwd)"/gradlew $GRADLE_TASKS
